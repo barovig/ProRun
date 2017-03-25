2a1580a78516e34ecf5f26d2ac1f7f1c0995bfe1
barovig <semargl1349@gmail.com>
2017-03-25 16:46:01 +0000

    Progress on CurrentActivity

--- mobile/src/main/java/com/mad/k00191419/prorun/activities/ProRunActivity.java
+++ mobile/src/main/java/com/mad/k00191419/prorun/activities/ProRunActivity.java
@@ -1,7 +1,9 @@
-package com.mad.k00191419.prorun;
+package com.mad.k00191419.prorun.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.mad.k00191419.prorun.R;
 
 public class ProRunActivity extends AppCompatActivity {
 
